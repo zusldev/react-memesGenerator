3430ccc399684369e4c5202c77798bc839979ac6
Liborzdev <liborio07zs@gmail.com>
2023-03-28 00:39:50 -0700

    Fixed img bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,28 @@
-import {useState} from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 import html2canvas from "html2canvas";
-
 function App() {
-    const [linea1, setLinea1] = useState('')
-    const [linea2, setLinea2] = useState('')
-    const [img, setImg] = useState('')
+    const [linea1, setLinea1] = useState("");
+    const [linea2, setLinea2] = useState("");
+    const [img, setImg] = useState("fire");
 
     const onChangeLinea1 = (e) => {
-        setLinea1(e.target.value)
-    }
+        setLinea1(e.target.value);
+    };
     const onChangeLinea2 = (e) => {
-        setLinea2(e.target.value)
-    }
-    const onChangeImg = (e) => {
-        setImg(e.target.value)
-    }
+        setLinea2(e.target.value);
+    };
     const exportarMeme = () => {
-        html2canvas(document.querySelector("#meme")).then(canvas => {
-            let link = document.createElement('a');
+        html2canvas(document.querySelector("#meme")).then((canvas) => {
+            let link = document.createElement("a");
             link.download = `meme${img}.png`;
-            link.href = canvas.toDataURL()
+            link.href = canvas.toDataURL();
             link.click();
         });
-    }
+    };
+    const onChangeImg = (e) => {
+        setImg(e.target.value);
+    };
     return (
         <div className="App">
             <div className="container">
@@ -40,24 +37,43 @@ function App() {
             </div>
 
             <div className="containerInput">
-                <input onChange={onChangeLinea1} type="text" placeholder="Your text here..."/>
-                <br/>
-                <input onChange={onChangeLinea2} type="text" placeholder="Your text here..."/>
-                <br/>
-                <br/>
-                <button onClick={exportarMeme}>Descargar</button>
+                <input
+                    onChange={onChangeLinea1}
+                    type="text"
+                    placeholder="Your text here..."
+                />
+                <br />
+                <input
+                    onChange={onChangeLinea2}
+                    type="text"
+                    placeholder="Your text here..."
+                />
+                <br />
+                <br />
+                <button onClick={exportarMeme}>Download</button>
             </div>
             <div className="meme" id="meme">
                 <h2>{linea1}</h2>
-                <img src={"public/" + img + ".jpg"} alt="description image to make a meme"/>
+                <img
+                    src={"src/assets/" + img + ".jpg"}
+                    alt="description image to make a meme"
+                />
                 <h2>{linea2}</h2>
             </div>
             <div className="footer">
-                <h3 className="madeBy">Made by: <a href='https://github.com/zusldev' target="_blank"
-                                                   rel="noopener noreferrer">@zusldev</a></h3>
+                <h3 className="madeBy">
+                    Made by:{" "}
+                    <a
+                        href="https://github.com/zusldev"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        @zusldev
+                    </a>
+                </h3>
             </div>
         </div>
-    )
+    );
 }
 
 export default App;
